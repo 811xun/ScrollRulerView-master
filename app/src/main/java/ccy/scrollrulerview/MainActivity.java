dedd139a21c488f060db3ed751399efd7f9ebcb4
811xun <zhichuan.xun@gotechcn.cn>
2019-03-08 09:05:02 +0800

    Initial commit

---
+++ app/src/main/java/ccy/scrollrulerview/MainActivity.java
@@ -0,0 +1,35 @@
+package ccy.scrollrulerview;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ScrollRulerView s2;
+    private ScrollRulerView s3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        s2 = (ScrollRulerView) findViewById(R.id.s2);
+        s2.setCurrentValue(-2.0f);
+
+        s3 = (ScrollRulerView) findViewById(R.id.s3);
+        s3.setEndNum(60);
+        s3.setMinGap(60);
+        s3.setUnit("厘米");
+        s3.setCurrentValue(35);
+        s3.setRulerBackground(0x33888888);
+        s3.setOnValueChangedListener(new ScrollRulerView.OnValueChangedListener() {
+            @Override
+            public void onValueChanged(float value) {
+                Log.d("ccy","current value = " + value);
+            }
+        });
+    }
+}
